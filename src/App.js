c5382b7058a39519ac87731f494bff39a1107a8f
Priyanka Chavan <picky.4494@gmail.com>
2021-01-13 17:51:08 +0530

    initial commit with complete todo-list app

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, {useState, useEffect, useRef} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import './App.css';
+
+function App() {
+	const firstRender = useRef(true);
+	const [inputValue, setInputValue] = useState('');
+	const [todos, setTodos] = useState([]);
+	const addTodo = (e) => {
+		e.preventDefault();
+
+		if (inputValue.trim() === '') return;
+
+		setTodos([...todos, {
+			text: inputValue,
+			id: uuidv4()
+		}]);
+
+		setInputValue('');
+	};
+
+	const removeTodo = (id) => {
+		setTodos(todos.filter(todo => todo.id !== id));
+	};
+
+	useEffect(() => {
+		if (firstRender.current) {
+			firstRender.current = false;
+		} else {
+			localStorage.setItem("Todo", JSON.stringify([...todos]));
+		}
+	}, [todos]);
+
+	useEffect(() => {
+		const newTodos = localStorage.getItem("Todo");
+		if (newTodos !== null) setTodos(JSON.parse([...todos, newTodos]));
+	}, []);
+
+	return (
+		<div className="App">
+			<div className="container">
+				<form onSubmit={addTodo}>
+					<input
+						autoFocus
+						type="text"
+						placeholder="Add a task..."
+						value={inputValue}
+						onChange={(e) => setInputValue(e.target.value)}
+					/>
+					<button type="submit">Add</button>
+				</form>
+				{
+					todos.map((todo) => (
+						<div key={todo.id} className="todo">
+							<p>{todo.text}</p>
+							<i onClick={() => removeTodo(todo.id)} className="fas fa-trash-alt"></i>
+						</div>
+					))
+				}
+			</div>
+		</div>
+	);
+}
+
+export default App;
